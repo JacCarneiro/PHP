79a238b8c2672d455b7e4d45dbe5579076f67a73
JacCarneiro <132837201+JacCarneiro@users.noreply.github.com>
2023-09-26 16:17:38 -0300

    update

---
+++ strings.php
@@ -0,0 +1,8 @@
+<?php
+    $idade = 21;
+    //exibição com aspas duplas
+    echo "Olá mundo!";
+    echo "Olá eu tenho $idade anos\n";
+
+    //exibição com aspas simples
+    echo 'Ola eu tenho '. $idade . ' anos'; 
