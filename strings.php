7a1da385d41f15fab88b984bebe4616cd9190f22
JacCarneiro <132837201+JacCarneiro@users.noreply.github.com>
2023-09-30 15:39:42 -0300

    update

--- strings.php
+++ strings.php
@@ -5,4 +5,15 @@
     echo "Olá eu tenho $idade anos\n" . PHP_EOL;
 
     //exibição com aspas simples
-    echo 'Ola eu tenho '. $idade . ' anos'; 
+    echo 'Ola eu tenho '. $idade . ' anos';
+
+    $idade = 16;
+
+    echo "Olá mundo!" . PHP_EOL;
+    echo "Eu tenho \"$idade\" anos";
+
+    echo PHP_EOL . PHP_EOL;
+
+    echo "Você só pode entrar, se tiver mais de 18 anos\n";
+
+    echo "Você tem $idade anos. Pode entrar";
