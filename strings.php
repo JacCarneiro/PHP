e3aa96072ec7755706bd54d64dd5cbef885f4439
JacCarneiro <132837201+JacCarneiro@users.noreply.github.com>
2023-09-30 14:10:45 -0300

    update strings

--- strings.php
+++ strings.php
@@ -2,7 +2,7 @@
     $idade = 21;
     //exibição com aspas duplas
     echo "Olá mundo!";
-    echo "Olá eu tenho $idade anos\n";
+    echo "Olá eu tenho $idade anos\n" . PHP_EOL;
 
     //exibição com aspas simples
     echo 'Ola eu tenho '. $idade . ' anos'; 
